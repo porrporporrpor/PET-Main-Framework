66fffe39e2a65a6fcd5ae8ef8b41df65b920d363
Benjawan Simpattanawong <benjawan.s@teohong.com>
2020-08-13 16:52:12 +0700

    add sql connection

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module gitlab.com/pplayground/pet_tracking/main-framework
 
 go 1.14
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/valyala/fasthttp v1.15.1
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
--- handler/repository/connectionDB.go
+++ handler/repository/connectionDB.go
@@ -1,14 +1,16 @@
 package repository
 
 import (
+	"database/sql"
 	"gitlab.com/pplayground/pet_tracking/main-framework/utils"
 	"gopkg.in/mgo.v2"
 	"log"
+	"os"
 )
 
 func ConnectMongoDB() (*mgo.Session, error) {
 	mongoDB := utils.GetConfig("MONGODB_HOST")
-	log.Printf("Connection to MongoDB %v connection\n", mongoDB)
+	log.Printf("Connecting to MongoDB %v connection\n", mongoDB)
 	session, err := mgo.Dial(mongoDB)
 	if err != nil {
 		return nil, err
@@ -21,4 +23,23 @@ func DisConnectMongoDB(session *mgo.Session) {
 	log.Println("Closing MongoDB connection")
 	session.Close()
 	log.Println("Closed MongoDB connection")
+}
+
+func ConnectSqlDB(dbName string) (*sql.DB, error) {
+	sqlDB := utils.GetConfig("MYSQLDB_HOST")
+	dbUser := os.Getenv("MYSQLDB_USER")
+	dbPwd := os.Getenv("MYSQLDB_PASSWORD")
+	log.Printf("Connecting to SqlDB %v connection\n", sqlDB)
+	db, err := sql.Open("mysql", dbUser+":"+dbPwd+"@tcp("+sqlDB+")/"+dbName)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Connected")
+	return db, nil
+}
+
+func DisConnectSqlDB(db *sql.DB) {
+	log.Println("Closing SqlDB connection")
+	db.Close()
+	log.Println("Closed SqlDB connection")
 }
